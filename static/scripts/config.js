f0fcaf79dbb1911343dec02b06fc18efb826cbb5
风轻云淡 <luping@shtel.com.cn>
2017-07-10 10:55:20 +0800

    Update config.js

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,1 +1,12 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = '118.89.144.148';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 9001;
+topic = '/esp8266';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+cleansession = true;
 
