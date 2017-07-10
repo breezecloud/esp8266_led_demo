19face6920d2daf386701242714148cb3613943f
风轻云淡 <luping@shtel.com.cn>
2017-07-10 11:03:46 +0800

    Update config.js

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,5 +1,5 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '118.89.144.148';	// hostname or IP address
+host = 'xx.xx.xx.xx';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
 port = 9001;
 topic = '/esp8266';		// topic to subscribe to
